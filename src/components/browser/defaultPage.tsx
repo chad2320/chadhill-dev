2af4c67925f7409bf883ebe6349623acc19aca1f
chad2320 <chadhill2.0@gmail.com>
2025-04-28 09:28:05 -0700

    Github Access Removed
    Deleted the github access token.

--- src/components/browser/defaultPage.tsx
+++ src/components/browser/defaultPage.tsx
@@ -51,26 +51,6 @@ export const DefaultPage: React.FC = () => {
     };
   });
 
-  const handleDownload = () => {
-    const documentUrl = "/chadHillResume.pdf";
-    const link = document.createElement("a");
-    link.href = documentUrl;
-
-    link.download = "chadHillResume.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  if (!repositoryInfo1 || !repositoryInfo2)
-    return (
-      <div className="flex h-full w-full items-center justify-center">
-        <h1 className="font-chicago text-2xl text-white">
-          Loading Github Info
-        </h1>
-      </div>
-    );
-
   return (
     <div
       ref={ref}
@@ -107,16 +87,17 @@ export const DefaultPage: React.FC = () => {
               width < 800 ? "text-sm" : "text-lg"
             } text-white`}
           >
-            {/* eslint-disable-next-line */}
-              I know a bit about webdev and decided to make this site as a fun little challenge for myself. I included another project below that I've worked on as it was an interest data processing problem I found.
+            {/* eslint-disable-next-line */}I know a bit about webdev and
+            decided to make this site as a fun little challenge for myself. I
+            included another project below that I've worked on as it was an
+            interest data processing problem I found.
           </p>
           <p
             className={` m-2 font-chicago ${
               width < 800 ? "text-sm" : "text-lg"
             } text-white`}
           >
-            {/* eslint-disable-next-line */}
-            I hope you found this interesting!
+            {/* eslint-disable-next-line */}I hope you found this interesting!
           </p>
           <p>{/* eslint-disable-next-line */}</p>
           <div
@@ -137,152 +118,163 @@ export const DefaultPage: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className=" mb-12 flex h-auto w-full flex-col items-center px-4">
-        <div className=" min-h-20 mb-2 flex w-[100%] max-w-[1000px] flex-col items-center rounded-md  bg-gradient-to-t from-slate-800 to-slate-700">
-          <div
-            className={` flex w-full ${
-              width > 700 ? "flex-row" : "flex-col"
-            } items-center justify-between`}
-          >
-            <h1 className="ml-2 font-chicago text-xl text-white">
-              Chads Data Tabler
-            </h1>
-            <p className="text-md font-chicago text-white">
-              Commits:&nbsp;{repositoryInfo1?.commits}
-            </p>
-            <p className="text-md font-chicago text-white">
-              Last Commit:&nbsp;
-              {dayjs().from(repositoryInfo1?.last_commit, true)}
-              &nbsp;ago
-            </p>
-            <div className="m-2 flex flex-row items-center">
-              <h1 className="font-chicago text-white">Github:&nbsp;</h1>
-              <button
-                className="rounded-sm p-[1px] text-white hover:bg-slate-500 "
-                onClick={() =>
-                  window.open("https://github.com/chad2320/Chads-Data-Tabler")
-                }
-              >
-                <LaunchIcon />
-              </button>
-            </div>
-          </div>
-          <div className="m-[3px] h-[2px] w-4/5 bg-black" />
-          <div className="flex w-full flex-col items-center justify-between">
-            <div className="w-[100%] p-2 font-chicago text-sm text-white">
-              <h1>
-                Automatically generate a viewable datatable with accurate
-                filters for each data type. Generally useful for webscraping
-                projects.
+      {repositoryInfo1 && repositoryInfo2 && (
+        <div className=" mb-12 flex h-auto w-full flex-col items-center px-4">
+          <div className=" min-h-20 mb-2 flex w-[100%] max-w-[1000px] flex-col items-center rounded-md  bg-gradient-to-t from-slate-800 to-slate-700">
+            <div
+              className={` flex w-full ${
+                width > 700 ? "flex-row" : "flex-col"
+              } items-center justify-between`}
+            >
+              <h1 className="ml-2 font-chicago text-xl text-white">
+                Chads Data Tabler
               </h1>
+              <p className="text-md font-chicago text-white">
+                Commits:&nbsp;{repositoryInfo1?.commits}
+              </p>
+              <p className="text-md font-chicago text-white">
+                Last Commit:&nbsp;
+                {dayjs().from(repositoryInfo1?.last_commit, true)}
+                &nbsp;ago
+              </p>
+              <div className="m-2 flex flex-row items-center">
+                <h1 className="font-chicago text-white">Github:&nbsp;</h1>
+                <button
+                  className="rounded-sm p-[1px] text-white hover:bg-slate-500 "
+                  onClick={() =>
+                    window.open("https://github.com/chad2320/Chads-Data-Tabler")
+                  }
+                >
+                  <LaunchIcon />
+                </button>
+              </div>
             </div>
-            <div
-              className={`flex w-full ${
-                width > 950 ? "flex-row" : "flex-col"
-              } justify-start font-chicago text-lg text-white`}
-            >
-              <LanguagesVisual languages={repositoryInfo1?.languages} />
+            <div className="m-[3px] h-[2px] w-4/5 bg-black" />
+            <div className="flex w-full flex-col items-center justify-between">
+              <div className="w-[100%] p-2 font-chicago text-sm text-white">
+                <h1>
+                  Automatically generate a viewable datatable with accurate
+                  filters for each data type. Generally useful for webscraping
+                  projects.
+                </h1>
+              </div>
               <div
                 className={`flex w-full ${
-                  width > 600 ? "flex-row" : "flex-col"
-                } justify-around`}
+                  width > 950 ? "flex-row" : "flex-col"
+                } justify-start font-chicago text-lg text-white`}
               >
-                <div className=" m-2 w-48">
-                  <h1>Frameworks:</h1>
-                  <ul className="text-sm">
-                    <li>React</li>
-                    <li>Mongoose</li>
-                    <li>Express</li>
-                  </ul>
+                <LanguagesVisual languages={repositoryInfo1?.languages} />
+                <div
+                  className={`flex w-full ${
+                    width > 600 ? "flex-row" : "flex-col"
+                  } justify-around`}
+                >
+                  <div className=" m-2 w-48">
+                    <h1>Frameworks:</h1>
+                    <ul className="text-sm">
+                      <li>React</li>
+                      <li>Mongoose</li>
+                      <li>Express</li>
+                    </ul>
+                  </div>
+                  <div className=" m-2 w-48">
+                    <h1>Libraries:</h1>
+                    <ul className="text-sm">
+                      <li>MUI</li>
+                    </ul>
+                  </div>
+                  <div className=" m-2 w-48">
+                    <h1>Platforms:</h1>
+                    <ul className="text-sm">
+                      <li>Atlas {"(Db)"}</li>
+                      <li>Vercel {"(FE/BE)"}</li>
+                    </ul>
+                  </div>
                 </div>
-                <div className=" m-2 w-48">
-                  <h1>Libraries:</h1>
-                  <ul className="text-sm">
-                    <li>MUI</li>
-                  </ul>
-                </div>
-                <div className=" m-2 w-48">
-                  <h1>Platforms:</h1>
-                  <ul className="text-sm">
-                    <li>Atlas {"(Db)"}</li>
-                    <li>Vercel {"(FE/BE)"}</li>
-                  </ul>
+              </div>
+            </div>
+          </div>
+          <div className=" min-h-20 mb-2 flex w-[100%] max-w-[1000px] flex-col items-center rounded-md  bg-gradient-to-t from-slate-800 to-slate-700">
+            <div
+              className={` flex w-full ${
+                width > 700 ? "flex-row" : "flex-col"
+              } items-center justify-between`}
+            >
+              <h1 className="ml-2 font-chicago text-xl text-white">
+                chadhill.dev
+              </h1>
+              <p className="text-md font-chicago text-white">
+                Commits:&nbsp;{repositoryInfo2?.commits}
+              </p>
+              <p className="text-md font-chicago text-white">
+                Last Commit:&nbsp;
+                {dayjs().from(repositoryInfo2?.last_commit, true)}
+                &nbsp;ago
+              </p>
+              <div className="m-2 flex flex-row items-center">
+                <h1 className="font-chicago text-white">Github:&nbsp;</h1>
+                <button
+                  className="rounded-sm p-[1px] text-white hover:bg-slate-500 "
+                  onClick={() =>
+                    window.open("https://github.com/chad2320/chadhill-dev")
+                  }
+                >
+                  <LaunchIcon />
+                </button>
+              </div>
+            </div>
+            <div className="m-[3px] h-[2px] w-4/5 bg-black" />
+            <div className="flex w-full flex-col items-center justify-between">
+              <div className="w-[100%] p-2 font-chicago text-sm text-white">
+                <h1>If your reading this, then you already know!</h1>
+              </div>
+              <div
+                className={`flex w-full ${
+                  width > 950 ? "flex-row" : "flex-col"
+                } justify-around font-chicago text-lg text-white`}
+              >
+                <LanguagesVisual languages={repositoryInfo2?.languages} />
+                <div
+                  className={`flex w-full ${
+                    width > 600 ? "flex-row" : "flex-col"
+                  } justify-around`}
+                >
+                  <div className=" m-2 w-48">
+                    <h1>Frameworks:</h1>
+                    <ul className="text-sm">
+                      <li>React</li>
+                    </ul>
+                  </div>
+                  <div className=" m-2 w-48">
+                    <h1>Libraries:</h1>
+                    <ul className="text-sm">
+                      <li>Tailwind</li>
+                      <li>Framer Motion</li>
+                      <li>Howler</li>
+                      <li>RND React</li>
+                    </ul>
+                  </div>
+                  <div className=" m-2 w-48">
+                    <h1>Platforms:</h1>
+                    <ul className="text-sm">
+                      <li>Vercel {"(FE)"}</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className=" min-h-20 mb-2 flex w-[100%] max-w-[1000px] flex-col items-center rounded-md  bg-gradient-to-t from-slate-800 to-slate-700">
-          <div
-            className={` flex w-full ${
-              width > 700 ? "flex-row" : "flex-col"
-            } items-center justify-between`}
-          >
-            <h1 className="ml-2 font-chicago text-xl text-white">
-              chadhill.dev
-            </h1>
-            <p className="text-md font-chicago text-white">
-              Commits:&nbsp;{repositoryInfo2?.commits}
-            </p>
-            <p className="text-md font-chicago text-white">
-              Last Commit:&nbsp;
-              {dayjs().from(repositoryInfo2?.last_commit, true)}
-              &nbsp;ago
-            </p>
-            <div className="m-2 flex flex-row items-center">
-              <h1 className="font-chicago text-white">Github:&nbsp;</h1>
-              <button
-                className="rounded-sm p-[1px] text-white hover:bg-slate-500 "
-                onClick={() =>
-                  window.open("https://github.com/chad2320/chadhill-dev")
-                }
-              >
-                <LaunchIcon />
-              </button>
-            </div>
-          </div>
-          <div className="m-[3px] h-[2px] w-4/5 bg-black" />
-          <div className="flex w-full flex-col items-center justify-between">
-            <div className="w-[100%] p-2 font-chicago text-sm text-white">
-              <h1>If your reading this, then you already know!</h1>
-            </div>
-            <div
-              className={`flex w-full ${
-                width > 950 ? "flex-row" : "flex-col"
-              } justify-around font-chicago text-lg text-white`}
-            >
-              <LanguagesVisual languages={repositoryInfo2?.languages} />
-              <div
-                className={`flex w-full ${
-                  width > 600 ? "flex-row" : "flex-col"
-                } justify-around`}
-              >
-                <div className=" m-2 w-48">
-                  <h1>Frameworks:</h1>
-                  <ul className="text-sm">
-                    <li>React</li>
-                  </ul>
-                </div>
-                <div className=" m-2 w-48">
-                  <h1>Libraries:</h1>
-                  <ul className="text-sm">
-                    <li>Tailwind</li>
-                    <li>Framer Motion</li>
-                    <li>Howler</li>
-                    <li>RND React</li>
-                  </ul>
-                </div>
-                <div className=" m-2 w-48">
-                  <h1>Platforms:</h1>
-                  <ul className="text-sm">
-                    <li>Vercel {"(FE)"}</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+      )}
+      {!repositoryInfo1 && (
+        <div className="flex h-full w-full items-center justify-center">
+          <h1 className="font-chicago text-2xl text-white">
+            I removed the github info for this project as it was a pain to
+            update.
+          </h1>
         </div>
-      </div>
+      )}
+      ``
     </div>
   );
 };
